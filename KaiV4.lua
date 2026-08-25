e360577a04ef6f2a7cf70353f3915ca299ffb0ec
mewAI36 <tranphuchung4069@gmail.com>
2026-08-25 18:36:29 +0700

    Update script key and load new Lua script

--- KaiV4.lua
+++ KaiV4.lua
@@ -1,2 +1,2 @@
-script_key = "trial"
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/4543b82035eecdd9eb412f827d823f91fb2f7c719c27e82b78d8af9ad7fbd0d2.lua"))()
+getgenv().SCRIPT_KEY = "791aa2b8-c4f2-4d54-9a82-aa789af0e17a"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/6ee322794d7238fe5faa613a55df34e8e5c1221766c5213bd4f8f40dd68d898a/download"))()
