829ce10fc8ecf845bd43d611fe284f360da724bc
mewAI36 <tranphuchung4069@gmail.com>
2026-08-31 16:26:13 +0700

    Update KaiV4.lua

--- KaiV4.lua
+++ KaiV4.lua
@@ -1,4 +1,4 @@
-getgenv().ditmemay = "95b161ed-fac5-4a6f-9db4-1e7df6e47e79"
+getgenv().ditmemay = "b7095248-d73f-405d-8ae9-4f44024ba2d4"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/37f5728a90ded56d68af0543dee0a08a7508c01e3e138f0459bc23be044a4c10/download"))()
 task.spawn(function()
     local CoreGui = game:GetService("CoreGui")
