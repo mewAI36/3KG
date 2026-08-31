a202c47591dcfeb4a151a415953ca95259fc4dd3
mewAI36 <tranphuchung4069@gmail.com>
2026-08-31 13:49:15 +0700

    Update SCRIPT_KEY in KaiV4.lua

--- KaiV4.lua
+++ KaiV4.lua
@@ -1,2 +1,2 @@
-getgenv().SCRIPT_KEY = "b7095248-d73f-405d-8ae9-4f44024ba2d4"
+getgenv().SCRIPT_KEY = "95b161ed-fac5-4a6f-9db4-1e7df6e47e79"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/37f5728a90ded56d68af0543dee0a08a7508c01e3e138f0459bc23be044a4c10/download"))()
