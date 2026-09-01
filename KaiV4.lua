c1df0200833aaa6cc7326f08b71ed11577c44a14
mewAI36 <tranphuchung4069@gmail.com>
2026-09-01 20:38:12 +0700

    Add loading of external Lua script from GitHub

--- KaiV4.lua
+++ KaiV4.lua
@@ -1,2 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mewAI36/3KG/refs/heads/main/cc3m.lua"))()
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/37f5728a90ded56d68af0543dee0a08a7508c01e3e138f0459bc23be044a4c10/download"))()
